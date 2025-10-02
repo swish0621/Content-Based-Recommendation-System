fd24e97372e73f565157f9ee19377638b9f53a87
swish0621 <swish0621@gmail.com>
2025-10-01 21:12:22 -0600

    fixed issues with movie data preprocessing

--- download_data.py
+++ download_data.py
@@ -3,12 +3,12 @@
 # pip install kagglehub[pandas-datasets]
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
-'''
+
 movies = kagglehub.dataset_load(
   KaggleDatasetAdapter.PANDAS,
   "rounakbanik/the-movies-dataset",
   "movies_metadata.csv", pandas_kwargs={"low_memory": True})
-'''
+
 keywords = kagglehub.dataset_load(
   KaggleDatasetAdapter.PANDAS,
   "rounakbanik/the-movies-dataset",
--- preprocess_data.py
+++ preprocess_data.py
@@ -38,9 +38,6 @@ keywords["key_space_sep"] = keywords["keyword_lists"].apply(lambda x: " ".join(x
 keyword_matrix = vectorizer.fit_transform(keywords["key_space_sep"])
 
 
-'''
-In the process of dealing with Series data types interfering with data transformations
-
 # remove excess columns from data 
 columns = [
     "id",
@@ -53,48 +50,38 @@ columns = [
 ]
 
 movies = movies[columns].copy()
-movies.rename(columns={"id": "ID"})
 
-# for use on columns with single object 
-def covert_meta(movie):
-    try:
-        metadata = ast.literal_eval(movie)
-        result = []
-        for item in metadata:
-            result.append(item[""])
-    except (ValueError):
-        return []
-    
-# for use on columns of metadata inclide json strings 
+# for use on columns of metadata include json strings 
 def json_col(col):
     try:
-        metadata = json.loads(col.replace(" ", ""))
+        if pd.isna(col) or col == "":
+            return []
+        try:
+            metadata = json.loads(col)
+        except json.JSONDecodeError:
+            metadata = ast.literal_eval(col)
+
         if isinstance(metadata, list):
-            return [item["name"].lower().replace(" ", "") for item in metadata]
+            return [item.get("name", "").lower().replace(" ", "") for item in metadata if "name" in item]
         elif isinstance(metadata, dict) and "name" in metadata:
             return [metadata["name"].lower().replace(" ", "")]
         else:
             return []
-    except (json.JSONDecodeError):
+    except (ValueError, TypeError, SyntaxError):
         return []
             
 def clean_movies(movies):
     movies["id"] = pd.to_numeric(movies["id"], errors="coerce")
-    movies["title"] = movies["title"].lower().replace(" ", "")
+    movies["title"] = movies["title"].str.lower().str.replace(" ", "")
     movies["genres"] = movies["genres"].apply(json_col)
-    movies["belongs_to_collection"] = movies["belongs_to_collection"].apply(lambda x: json_col(x))
-    movies["original_language"] = movies["original_language"].lower().replace(" ", "")
-    movies["popularity"] = movies["popularity"].astype(int)
-    movies["production_companies"] = movies["production_companies"].apply(lambda x: json_col(x))
+    movies["belongs_to_collection"] = movies["belongs_to_collection"].apply(json_col)
+    movies["original_language"] = movies["original_language"].str.lower().str.replace(" ", "")
+    movies["popularity"] = pd.to_numeric(movies["popularity"], errors="coerce").fillna(0).astype(int)
+    movies["production_companies"] = movies["production_companies"].apply(json_col)
 
-clean_movies(movies)
-if movies["id"] is not isinstance(movies["id"], int):
-    print("soemthing isnt an int")
-else:
-    pass 
-#clean_movies(movies)
-#print(movies.head())
-'''
-            
-            
+    return movies
+
+movies = clean_movies(movies)
+print(movies.head(5))
+
 
