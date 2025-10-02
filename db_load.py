b0a1c817dc948997e59646aa4d826ff336466e55
swish0621 <swish0621@gmail.com>
2025-10-02 14:33:40 -0600

    load movies and keywords into db

---
+++ db_load.py
@@ -0,0 +1,59 @@
+import sqlite3
+from preprocess_data import movies, keywords
+from db_crud import *
+
+
+# load movies and their data into the database 
+def load_movies():
+    for _, row in movies.iterrows():
+        # add movie
+        movie_id = add_movie(row["title"], row["original_language"], row["popularity"])
+
+        # insert genres and link
+        for genre_name in row["genres"]:
+            genre = get_genre_by_name(genre_name)
+            if not genre:
+                genre_id = add_genre(genre_name)
+            else:
+                genre_id = genre[0]
+            link_movie_to_genre(movie_id, genre_id)
+
+        # insert companies and link 
+        for company_name in row["production_companies"]:
+            company = get_company_by_name(company_name)
+            if not company:
+                company_id = add_production_company(company_name)
+            else:
+                company_id = company[0]
+            link_movie_to_company(movie_id, company_id)
+
+        # insert collections and link 
+        for collection_name in row["belongs_to_collection"]:
+            collection = get_collection_by_name(collection_name)
+            if not collection:
+                collection_id = add_collection(collection_name)
+            else:
+                collection_id = collection[0]
+            link_movie_to_collection(movie_id, collection_id)
+
+    conn.commit()
+
+def load_keywords():
+    for _, row in keywords.iterrows():
+
+        # adding movie id
+        movie_id = row["id"]
+
+        # insert keywords and link 
+        for keyword_name in row["keyword_lists"]:
+            keyword = get_keyword_by_name(keyword_name)
+            if not keyword:
+                keyword_id = add_keyword(keyword_name)
+            else: 
+                keyword_id = keyword[0]
+            link_movie_to_keyword(movie_id, keyword_id)
+    conn.commit()
+            
+load_movies()
+load_keywords()
+
