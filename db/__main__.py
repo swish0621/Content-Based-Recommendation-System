beb18f1c94f0705c2f5734bdf726edb35417cd5f
swish0621 <swish0621@gmail.com>
2025-11-12 00:08:32 -0700

    limit rebuilding if db exists

--- db/__main__.py
+++ db/__main__.py
@@ -2,15 +2,17 @@ import sqlite3
 from preprocess_data import movies, keywords
 from db.db_setup import create_database
 from db.db_load import load_keywords, load_movies
+import os
 
 
 def run_db_pipeline():
-    create_database()
-    conn = sqlite3.connect("movies.db")
-    load_movies(conn, movies)
-    load_keywords(conn, keywords)
-    conn.close()
-    print("Database setup / load complete")
+    if not os.path.exists("movies.db"):
+        create_database()
+        conn = sqlite3.connect("movies.db")
+        load_movies(conn, movies)
+        load_keywords(conn, keywords)
+        conn.close()
+        print("Database setup / load complete")
 
 if __name__ == "__main__":
     run_db_pipeline()
