302aaa5015504ddc14eb9e4e36e320d182a8e3b8
swish0621 <swish0621@gmail.com>
2026-02-03 14:57:01 -0700

    file restructure: move main.py into backend directory and create module

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI, Request, Form, Query
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi import Body
 import sqlite3
-from feature.recommender import get_recommendations
+from backend.feature.recommender import get_recommendations
 
 
 app = FastAPI()
@@ -15,8 +16,8 @@ def home(request: Request):
         return templates.TemplateResponse("index.html", {"request": request})
 
 # Calls the get_recommendations function using selected movies and outputs the results onto the recommendations template 
-@app.post("/recommend", response_class=HTMLResponse)
-async def recommend(request: Request, movie_ids: list[int] = Form(...)):
+@app.post("/recommend")
+async def recommend(movie_ids: list[int] = Body(...)):
     recommendations = get_recommendations(movie_ids)
     conn = sqlite3.connect("movies.db")
     cursor = conn.cursor()
@@ -26,27 +27,24 @@ async def recommend(request: Request, movie_ids: list[int] = Form(...)):
     cursor.execute(f"SELECT movie_id, original_title FROM movie WHERE movie_id In ({placeholders})", recommendations["movie_id"].tolist())
     title_map = dict(cursor.fetchall())
     conn.close()
+    
     # Transform results into jinja compliant format 
     recommendations["original_title"] = recommendations["movie_id"].map(title_map)
-    recommendations = recommendations[["original_title", "similarity"]]
-
-    return templates.TemplateResponse(
-        "recommendations.html", 
-        {"request": request, "recommendations": recommendations.to_dict(orient="records")}
-    )
+    
+    return recommendations[["original_title", "similarity"]].to_dict(orient="records")
 
 # Returns movie titles that are similar to the searched for title 
-@app.get("/search_movies")
-def search_movies(q: str = Query("", min_length = 1, desc="Movie search Query")):
+@app.get("/search")
+def search_movies(q: str = Query(..., min_length = 1, description="Movie search Query")):
     conn = sqlite3.connect("movies.db")
     cursor = conn.cursor()
     cursor.execute('''
                     SELECT movie_id, original_title
                     FROM movie 
-                    WHERE LOWER(original_title) LIKE LOWER(?)
+                    WHERE LOWER(original_title) LIKE ?
                     ORDER BY popularity DESC
                     LIMIT 5
-                   ''', (f"%{q.replace(' ', '').lower()}%",) )
+                   ''', (f"%{q.lower()}%",) )
     results = cursor.fetchall()
     conn.close()
 
